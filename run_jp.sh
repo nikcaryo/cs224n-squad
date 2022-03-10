cc1528abaa0b89b8a6e20f43584e2bdb95c707d1
JP Reilly <jpreilly@stanford.edu>
2022-03-10 09:14:08 -0800

    new jp sh"

--- run_jp.sh
+++ run_jp.sh
@@ -1,4 +1,4 @@
-python3 train.py --name 'CBB_LR_0.5_HIDDEN_100_DROP_0.5' --model 'charbidaf' --lr 0.5 --use_char true --output bidaf --attention bidaf --drop_prob 0.5
+# python3 train.py --name 'CBB_LR_0.5_HIDDEN_100_DROP_0.5' --model 'charbidaf' --lr 0.5 --use_char true --output bidaf --attention bidaf --drop_prob 0.5
 python3 train.py --name 'BSB_LR_0.5_HIDDEN_100_DROP_0.5' --model 'charbidaf' --lr 0.5 --use_char false --output bidaf --attention rnet --drop_prob 0.5
 python3 train.py --name 'BBR_LR_0.5_HIDDEN_100_DROP_0.5' --model 'charbidaf' --lr 0.5 --use_char false --output rnet --attention bidaf --drop_prob 0.5
 python3 train.py --name 'CSR_LR_0.5_HIDDEN_100_DROP_0.5' --model 'charbidaf' --lr 0.5 --use_char true --output rnet --attention rnet --drop_prob 0.5
