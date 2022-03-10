b1e16a17855af2912370d5b0b21d9fb106a70d97
JP Reilly <jpreilly@stanford.edu>
2022-03-09 17:42:31 -0800

    hey

--- raytrain.py
+++ raytrain.py
@@ -34,7 +34,7 @@ def get_absolute(filepath):
     device, args.gpu_ids = util.get_available_devices()
     print(device)
     if device != 'cpu':
-        return filepath
+        return filepath[1:]
     else:
         return '/Users/{}/cs224n-squad'.format(USERNAME) + filepath[1:]
 
